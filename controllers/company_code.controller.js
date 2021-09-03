35f108ea39f68bb4300d894d983339ab2e3d70b2
SJPark <sjpark@mslm.kr>
2021-09-03 19:03:03 +0900

    company_code CRUD

--- controllers/company_code.controller.js
+++ controllers/company_code.controller.js
@@ -7,27 +7,44 @@ exports.company_code_Read = (req,res) => {
   })
 }
 exports.company_code_Create = (req,res) =>{
-  company_code_Model.company_code_Create(req.body["company-code"],(err,data)=>{
+  company_code_Model.company_code_Create(req.body["company-code-create"],(err,data)=>{
     var res_body = new Object()
-    res_body["company-code-resp"] = new Object()
+    res_body["company-code-create-resp"] = new Object()
     if(err){
-      res_body["company-code-resp"]["result"]=err.result
+      res_body["company-code-create-resp"]["result"]=err.result
       res.status(500).send(res_body)
     }else{
-      res_body["company-code-resp"]["result"]=data.result
+      res_body["company-code-create-resp"]["result"]=data.result
       res.send(res_body)
     }
   })
 }
 exports.company_code_Update = (req,res) =>{
-  console.log("post company_code_Create controller")
-  company_code_Model.company_code_Update(req,(err,data)=>{
-    res.send(data)
+  console.log("patch company_code_Update controller")
+  company_code_Model.company_code_Update(req.body["company-code-update"],(err,data)=>{
+    var res_body = new Object()
+    res_body["company-code-update-resp"] = new Object()
+    if(err){
+      res_body["company-code-update-resp"]["result"]=err.result
+      res.status(500).send(res_body)
+    }else{
+      res_body["company-code-update-resp"]["result"]=data.result
+      res.send(res_body)
+    }
   })
 }
+
 exports.company_code_Delete = (req,res) =>{
-  console.log("post company_code_Create controller")
-  company_code_Model.company_code_Delete(req,(err,data)=>[
-    res.send(data)
-  ])
+  console.log("delete company_code_Delete controller : ",req.body)
+  company_code_Model.company_code_Delete(req.body["company-code-delete"],(err,data)=>{
+    var res_body = new Object()
+    res_body["company-code-delete-resp"] = new Object()
+    if(err){
+      res_body["company-code-delete-resp"]["result"]=err.result
+      res.status(500).send(res_body)
+    }else{
+      res_body["company-code-delete-resp"]["result"]=data.result
+      res.send(res_body)
+    }
+  })
 }
--- models/company_code.model.js
+++ models/company_code.model.js
@@ -1,30 +1,51 @@
 const sql = require("../config/db.js");
 
-const company_code = () => {}
+const company_code = () => { }
 
 company_code.company_code_Read = (req, result) => {
-  sql.query("SELECT * FROM company_code",(err,res) => {
-    result(null,res)
+  sql.query("SELECT * FROM company_code", (err, res) => {
+    result(null, res)
   })
 }
-company_code.company_code_Create = (req,result) =>{
-  var values = req.map(d=>{
-    return [d.code_value,d.code_id,d.code_name,d.code_description,d.code_option,d.code_sort]
+company_code.company_code_Create = (req, result) => {
+  var values = req.map(d => {
+    return [d.code_value, d.code_id, d.code_name, d.code_description, d.code_option, d.code_sort]
   })
-  console.log("values: ",values)
-  sql.query("INSERT INTO company_code(code_value,code_id,code_name,code_description,code_option,code_sort) VALUES ?",[values],
-  (err,res)=>{
-    if(err)
-      result({result : "COMPANY-CODE-DB-FAIL"},null)
-    else 
-      result(null, {result : "COMPANY-CODE-CREATE-OK"})
+  console.log("values: ", values)
+  sql.query("INSERT INTO company_code(code_value,code_id,code_name,code_description,code_option,code_sort) VALUES ?", [values],
+    (err, res) => {
+      if (err)
+        result({ result: "COMPANY-CODE-DB-FAIL" }, null)
+      else
+        result(null, { result: "COMPANY-CODE-CREATE-OK" })
+    })
+}
+company_code.company_code_Update = (req, result) => {
+  console.log("code_name : ", req.code_name)
+  console.log("code_description : ", req.code_description)
+  console.log("code_sort : ", req.code_sort)
+  console.log("code_value : ", req.code_value)
+  sql.query("UPDATE company_code SET code_name=?,code_description=?,code_sort=? WHERE code_value=?",
+    [req.code_name, req.code_description, req.code_sort, req.code_value],
+    (err, res) => {
+      if (err)
+        result({ result: "COMPANY-CODE-UPDATE-DB-FAIL" }, null)
+      else if (res.affectedRows === 0)
+        result({ result: "COMPANY-CODE-UPDATE-DB-NOT-FOUND" }, null)
+      else
+        result(null, { result: "COMPANY-CODE-UPDATE-OK" })
+    })
+}
+company_code.company_code_Delete = (req, result) => {
+  console.log("delete req : ", req)
+  sql.query("DELETE FROM company_code WHERE code_value in (?)", [req], (err, res) => {
+    if (err)
+      result({ result: "COMPANY-CODE-DELETE-DB-FAIL" },null)
+    else if (res.affectedRows === 0)
+      result({result: "COMPANY-CODE-DELETE-DB-NOT-FOUND"},null)
+    else
+      result(null,{result : "COMPANY-CODE-OK"})
   })
-}
-company_code.company_code_Update = (req,result) =>{
-  //sql result
-}
-company_code.company_code_Delete = (req,result) =>{
-  //sql result
 }
 
 
